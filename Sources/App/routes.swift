bd3d44649ecced17897b54d53ea49c1002ea701a
Petre Vane <PetreVane@users.noreply.github.com>
2019-12-17 18:26:24 +0100

    Added Leaf as dependency
    Added Leaf as template; added new route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,8 +4,16 @@ import Vapor
 /// Register your application's routes here.
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
+
 public func routes(_ router: Router) throws {
+    
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    router.get() { request in
+        return "Hello there, this is the starting page!"
+    }
+    
+    
 }
